aa5d614e4efab6f78ac06f142fdab7404aac9f53
nikita gaidamachenko <gaidamac_n@qwasar.io>
2023-07-28 21:22:56 +0000

    added test cases

--- my_printf.c
+++ my_printf.c
@@ -72,3 +72,23 @@ void my_printf(char* specification, ...){
     }
     va_end(args);
 }
+
+int main() {
+    int nbr = 65;
+    unsigned int unnbr = 0;
+    unsigned int unnbr_1 = 15;
+    unsigned int unnbr_2 = 48;
+    char c = 'A';
+    char* str = "Hello World\n";
+    void* ptr = &nbr;
+
+    my_printf("Signed Integer: %d\n", nbr);
+    my_printf("Unsigned octal: %o\n", unnbr);
+    my_printf("Unsigned octal: %o\n", unnbr_2);
+    my_printf("Unsigned decimal: %u\n", unnbr);
+    my_printf("Unsigned hex: %x\n", unnbr_1);
+    my_printf("Unsigned hex: %x\n", unnbr_2);
+    my_printf("Char: %c\n", c);
+    my_printf("String: %s\n", str);
+    my_printf("The Holy Pointer: %p\n", ptr);
+}
