c728c72dff93a848d44f09b56f0419a9b5288889
nikita gaidamachenko <gaidamac_n@qwasar.io>
2023-07-29 04:31:01 +0000

    making changes to printf to return strlen

--- my_printf.c
+++ my_printf.c
@@ -15,7 +15,12 @@
 Try using itoa to convert intergers into a string
 */
 
-#include "my_printf.h"
+
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include <stdint.h>
+
 
 void reverse_string(char* str){
     int i, j; //variables from both ends iterating through the string
@@ -133,9 +138,10 @@ void my_signed_bin(unsigned int nbr){ //binary
     my_putstr(buffer);
 }
 
-void my_printf(char* specification, ...){
+int my_printf(char* specification, ...){
     va_list args;
     va_start(args, specification);
+    int total_chars = 0;
     while(*specification != '\0'){
         if(*specification == '%'){
             specification++;
@@ -143,48 +149,59 @@ void my_printf(char* specification, ...){
                 case 'd': {
                     int argument = va_arg(args, int); // need to convert doux from int into a string
                     my_signed_decimal(argument);
+                    total_chars ++;
                     break;
                 }
                 case 'o': {
                     unsigned int argument = va_arg(args, unsigned int);
                     my_unsigned_oct(argument);
+                    total_chars ++;
                     break;
                 }
                 case 'u': {
                     unsigned int argument = va_arg(args, unsigned int);
                     my_unsigned_decimal(argument);
+                    total_chars ++;
                     break;
                 }
                 case 'x': {
                     unsigned int argument = va_arg(args, unsigned int);
                     my_unsigned_hex(argument);
+                    total_chars ++;
                     break;
                 }
                 case 'c': {
                     char argument = (char)va_arg(args, int);
                     my_putchar(argument);
+                    total_chars++;
                     break;
                 }
                 case 's': {
                     char* argument = va_arg(args, char*);
                     my_putstr(argument);
+                    total_chars += strlen(argument);
                     break;
                 }
                 case 'p': {
                     void* argument = va_arg(args, void*);
                     my_putstr("0x"); // to indicate that it's a hex representation of a pointer
                     my_unsigned_hex((unsigned long int)argument);
+                    total_chars += strlen(argument);
                     break;
                 }
                 default:{
                     my_putchar(*specification);
+                    total_chars++;
+                    break;
                 }
             }
         }
         else{
             my_putchar(*specification);
+            total_chars++;
         }
         specification++;
     }
     va_end(args);
+    return total_chars;
 }
